599dc4b76eb1004a62e1a4939247433e1ae2dd39
Tom Gregor <tgregor@ivsoftware.com>
2026-03-17 03:35:10 -0600

    MOVED: RouteToFullRecordset

--- IVSoftware.Portable.SQLiteMarkdown/MarkdownContext.cs
+++ IVSoftware.Portable.SQLiteMarkdown/MarkdownContext.cs
@@ -1635,33 +1635,6 @@ namespace IVSoftware.Portable.SQLiteMarkdown
         protected bool _isFiltering = false;
 
         /// <summary>
-        /// True when InputText is empty regardless of IsFiltering.
-        /// </summary>
-        /// <remarks>
-        /// Mental Model:
-        /// "If the input text is empty, just swap the handle instead of recalculating."
-        /// Functional Behavior:
-        /// - External predicate filters must still run even if IME doesn't contribute.
-        /// - This is the purview of the subclass. Override for full control.
-        /// </remarks>
-        public virtual bool RouteToFullRecordset
-        {
-            get
-            {
-                if(InputText.Trim().Length == 0)
-                {
-                    return true;
-                }
-                int
-                    autocount = Model.GetAttributeValue<int>(StdMarkdownAttribute.autocount, 0),
-                    matches = Model.GetAttributeValue<int>(StdMarkdownAttribute.matches, 0);
-                return autocount == matches;
-            }
-        }
-
-        bool _routeToFullRecordset = true;
-
-        /// <summary>
         /// Catch and release heuristic for canonical ObservableNetProjection entering and leaving IsFiltered state.
         /// </summary>
         protected virtual void OnIsFilteringChanged() { }
--- IVSoftware.Portable.SQLiteMarkdown/ModeledMarkdownContext.T.cs
+++ IVSoftware.Portable.SQLiteMarkdown/ModeledMarkdownContext.T.cs
@@ -309,6 +309,32 @@ namespace IVSoftware.Portable.SQLiteMarkdown
             #endregion L o c a l F x
         }
 
+        /// <summary>
+        /// True when InputText is empty regardless of IsFiltering.
+        /// </summary>
+        /// <remarks>
+        /// Mental Model:
+        /// "If the input text is empty, just swap the handle instead of recalculating."
+        /// Functional Behavior:
+        /// - External predicate filters must still run even if IME doesn't contribute.
+        /// - This is the purview of the subclass. Override for full control.
+        /// </remarks>
+        public virtual bool RouteToFullRecordset
+        {
+            get
+            {
+                if (InputText.Trim().Length == 0)
+                {
+                    return true;
+                }
+                int
+                    autocount = Model.GetAttributeValue<int>(StdMarkdownAttribute.autocount, 0),
+                    matches = Model.GetAttributeValue<int>(StdMarkdownAttribute.matches, 0);
+                return autocount == matches;
+            }
+        }
+
+        bool _routeToFullRecordset = true;
 
 
         /// <summary>
