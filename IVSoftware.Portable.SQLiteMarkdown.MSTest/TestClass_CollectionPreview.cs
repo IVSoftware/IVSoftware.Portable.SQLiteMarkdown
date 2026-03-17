2a72bb5cb3c0311b76fc14d3be66d4f7ed813743
Tom Gregor <tgregor@ivsoftware.com>
2026-03-17 14:26:43 -0600

    [Probationary] authority scheme

--- IVSoftware.Portable.SQLiteMarkdown.MSTest/TestClass_CollectionPreview.cs
+++ IVSoftware.Portable.SQLiteMarkdown.MSTest/TestClass_CollectionPreview.cs
@@ -1,6 +1,8 @@
 using IVSoftware.Portable.SQLiteMarkdown.MSTest.Models;
 using IVSoftware.Portable.SQLiteMarkdown.Util;
 using IVSoftware.Portable.Xml.Linq.XBoundObject;
+using IVSoftware.WinOS.MSTest.Extensions;
+using Newtonsoft.Json;
 using SQLite;
 using System.Collections.ObjectModel;
 
@@ -9,9 +11,13 @@ namespace IVSoftware.Portable.SQLiteMarkdown.MSTest
     [TestClass]
     public class TestClass_CollectionPreview
     {
-        [TestMethod]
+        [TestMethod, DoNotParallelize]
         public void TestMethod_AddRange()
         {
+            using var te = this.TestableEpoch();
+
+            string actual, expected;
+
             Switcheroo.ObservableNetProjectionWithComposition<ItemCardModel>
                 opc = new();
             IModeledMarkdownContext
@@ -39,6 +45,34 @@ namespace IVSoftware.Portable.SQLiteMarkdown.MSTest
 
             opc.AddDynamic("Brown Dog", "[canine][color]", false, new() { "loyal", "friend", "furry" });
             { }
+
+            actual = JsonConvert.SerializeObject(opc, Formatting.Indented);
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+[
+  {
+    ""Id"": ""312d1c21-0000-0000-0000-000000000000"",
+    ""Description"": ""Brown Dog"",
+    ""Keywords"": ""[\""loyal\"",\""friend\"",\""furry\""]"",
+    ""KeywordsDisplay"": ""\""loyal\"",\""friend\"",\""furry\"""",
+    ""Tags"": ""[canine] [color]"",
+    ""IsChecked"": false,
+    ""Selection"": 0,
+    ""IsEditing"": false,
+    ""PrimaryKey"": ""312d1c21-0000-0000-0000-000000000000"",
+    ""QueryTerm"": ""brown~dog~loyal~friend~furry~[canine]~[color]"",
+    ""FilterTerm"": ""brown~dog~loyal~friend~furry~[canine]~[color]"",
+    ""TagMatchTerm"": ""[canine] [color]"",
+    ""Properties"": ""{\r\n  \""Description\"": \""Brown Dog\"",\r\n  \""Tags\"": \""[canine] [color]\"",\r\n  \""Keywords\"": \""[\\\""loyal\\\"",\\\""friend\\\"",\\\""furry\\\""]\""\r\n}""
+  }
+]";
+
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting model to track."
+            );
         }
     }
 }
