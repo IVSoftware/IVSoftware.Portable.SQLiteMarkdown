8a2d6bd4b5dda19c0c09babae3ca88ddb13fa24b
Tom Gregor <tgregor@ivsoftware.com>
2026-03-22 09:17:39 -0600

    ORIGIN: DHostBatchCollectionChange with scaffold

--- IVSoftware.Portable.SQLiteMarkdown/Collections/Preview/DHostBatchCollectionChange.cs
+++ IVSoftware.Portable.SQLiteMarkdown/Collections/Preview/DHostBatchCollectionChange.cs
@@ -1,22 +1,55 @@
-﻿using IVSoftware.Portable.Disposable;
+﻿using IVSoftware.Portable.Common.Attributes;
+using IVSoftware.Portable.Disposable;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Text;
 
 namespace IVSoftware.Portable.SQLiteMarkdown.Collections.Preview
 {
     internal class DHostBatchCollectionChange : DisposableHost
     {
-        public NotifyCollectionChangingEventArgs eBatch = new (
-                action: NotifyCollectionChangeAction.Replace,                
-                reason: NotifyCollectionChangeReason.Batch,
-                oldItems: new List<CollectionChangingEventArgs>(),
-                newItems: new List<CollectionChangingEventArgs>());
+        INotifyCollectionChanged _listB4 = null!;
+        IList _listFTR = null!;
         protected override void OnBeginUsing(BeginUsingEventArgs e)
         {
             base.OnBeginUsing(e);
-            eBatch.OldItems!.Clear();
-            eBatch.NewItems!.Clear();
         }
+        protected override void OnFinalDispose(FinalDisposeEventArgs e)
+        {
+            base.OnFinalDispose(e);
+        }
+
+        public new IDisposable GetToken(object? sender = null, Dictionary<string, object>? properties = null)
+        {
+            throw new NotImplementedException("ToDo");
+        }
+
+        public new IDisposable GetToken(string key, object value)
+        {
+            throw new NotImplementedException("ToDo");
+        }
+        public new IDisposable GetToken(object sender, string key, object value)
+        {
+            throw new NotImplementedException("ToDo");
+        }
+
+        [Canonical]
+        public IDisposable GetToken(INotifyCollectionChanged listB4)
+        {
+            _listB4 = listB4;
+            return base.GetToken();
+        }
+    internal class BatchFinalDisposeEventArgs : FinalDisposeEventArgs
+    {
+        public BatchFinalDisposeEventArgs(
+            IReadOnlyCollection<object> releasedSenders, 
+            IReadOnlyDictionary<string, object> snapshot) : base(releasedSenders, snapshot)
+        {
+            throw new NotImplementedException("ToDo");
+        }
+
+        public NotifyCollectionChangingEventArgs BatchEventArgs { get; }
     }
 }
