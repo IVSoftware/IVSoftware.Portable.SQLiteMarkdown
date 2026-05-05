c9c469a398e3803cbe101bf188cbbab963070b34
Tom Gregor <tgregor@ivsoftware.com>
2026-05-05 17:11:33 -0600

    @23

--- IVSoftware.Portable.SQLiteMarkdown/Obsolete/Interface.cs
+++ IVSoftware.Portable.SQLiteMarkdown/Obsolete/Interface.cs
@@ -158,6 +158,76 @@ namespace IVSoftware.Portable.SQLiteMarkdown.Obsolete
         string[] GetTableNames();
     }
 
+    /// <summary>
+    /// Extends MarkdownContext with a predicate AND clause that is property-based.
+    /// </summary>
+    /// <remarks>
+    /// Query and filter modes are equally affected.
+    /// EXAMPLE:
+    /// Activating this StdPredicate value will hide checked items in queries 
+    /// and filters, and checking the item in the UI will filter that item out.
+    /// <c>
+    /// [Where("IsChecked", WherePredicate.IsFalse)]
+    /// IsUnchecked,
+    /// </c>
+    /// </remarks>
+    [Probationary("Maintain as Internal until stable.")]
+    [PublishedContract("2.0.0-alpha31", typeof(IPredicateMarkdownContext))]
+    [Careful("This interface is not allowed to implement INotifyCollectionChanged.")]
+    public interface IPredicateMarkdownContext : IMarkdownContext
+    {
+        /// <summary>
+        /// Obtain a token that suspends updates.
+        /// </summary>
+        /// <remarks>
+        /// Avoids intermediate transitions when multiple predicates change state 
+        /// simultaneously (e.g., ShowChecked v ShowUnchecked radio buttons).
+        /// </remarks>
+        IDisposable BeginPredicateAtom();
+
+        IReadOnlyDictionary<string, Enum> ActiveFilters { get; }
+
+        void ActivatePredicates(Enum stdPredicate, params Enum[] more);
+
+        void DeactivatePredicates(Enum stdPredicate, params Enum[] more);
+
+        void ClearPredicates(bool clearInputText = true);
+    }
+    public interface ITopology
+    {
+        #region C O N F I G U R A T I O N    P R O P E R T I E S
+        /// <summary>
+        /// OPT-IN that allows MarkdownContext to modify the ObservableNetCollection directly.
+        /// </summary>
+        NetProjectionTopology ProjectionTopology { get; }
+
+        /// <summary>
+        /// Determines whether filter update events are provided as structural changes
+        /// with old-new item semantics, alternatively as a bulk reset, or both.
+        /// </summary>
+        /// <remarks>
+        /// Some UI platforms respond more efficiently to a raw reset.
+        /// </remarks>
+
+        [Careful("Not to be conflated with CollectionChangingEventingPolicy")]
+        ReplaceItemsEventingPolicy ReplaceItemsEventingPolicy { get; set; }
+        #endregion C O N F I G U R A T I O N    P R O P E R T I E S
+
+        #region P R O J E C T I O N
+        /// <summary>
+        /// Represents a bindable and observable collection representing 'net visible' filtered items.
+        /// </summary>
+        IList? ObservableNetProjection { get; }
+
+        public IList CanonicalSuperset { get; }
+
+        public IList PredicateMatchSubset { get; }
+        #endregion  P R O J E C T I O N
+
+        public int Count { get; }
+    }
+
+#if false
     public interface IModeledMarkdownContext
         : IMarkdownContext
         , ITopology
@@ -225,39 +295,6 @@ namespace IVSoftware.Portable.SQLiteMarkdown.Obsolete
 
         #endregion D I S P O S A B L E
     }
-    public interface ITopology
-    {
-        #region C O N F I G U R A T I O N    P R O P E R T I E S
-        /// <summary>
-        /// OPT-IN that allows MarkdownContext to modify the ObservableNetCollection directly.
-        /// </summary>
-        NetProjectionTopology ProjectionTopology { get; }
-
-        /// <summary>
-        /// Determines whether filter update events are provided as structural changes
-        /// with old-new item semantics, alternatively as a bulk reset, or both.
-        /// </summary>
-        /// <remarks>
-        /// Some UI platforms respond more efficiently to a raw reset.
-        /// </remarks>
-
-        [Careful("Not to be conflated with CollectionChangingEventingPolicy")]
-        ReplaceItemsEventingPolicy ReplaceItemsEventingPolicy { get; set; }
-        #endregion C O N F I G U R A T I O N    P R O P E R T I E S
-
-        #region P R O J E C T I O N
-        /// <summary>
-        /// Represents a bindable and observable collection representing 'net visible' filtered items.
-        /// </summary>
-        IList? ObservableNetProjection { get; }
-
-        public IList CanonicalSuperset { get; }
-
-        public IList PredicateMatchSubset { get; }
-        #endregion  P R O J E C T I O N
-
-        public int Count { get; }
-    }
     public interface IModeledMarkdownContext<T> : IModeledMarkdownContext
     {
         /// <summary>
@@ -272,40 +309,5 @@ namespace IVSoftware.Portable.SQLiteMarkdown.Obsolete
 
         new IReadOnlyList<T> PredicateMatchSubset { get; }
     }
-
-    /// <summary>
-    /// Extends MarkdownContext with a predicate AND clause that is property-based.
-    /// </summary>
-    /// <remarks>
-    /// Query and filter modes are equally affected.
-    /// EXAMPLE:
-    /// Activating this StdPredicate value will hide checked items in queries 
-    /// and filters, and checking the item in the UI will filter that item out.
-    /// <c>
-    /// [Where("IsChecked", WherePredicate.IsFalse)]
-    /// IsUnchecked,
-    /// </c>
-    /// </remarks>
-    [Probationary("Maintain as Internal until stable.")]
-    [PublishedContract("2.0.0-alpha31", typeof(IPredicateMarkdownContext))]
-    [Careful("This interface is not allowed to implement INotifyCollectionChanged.")]
-    public interface IPredicateMarkdownContext : IMarkdownContext
-    {
-        /// <summary>
-        /// Obtain a token that suspends updates.
-        /// </summary>
-        /// <remarks>
-        /// Avoids intermediate transitions when multiple predicates change state 
-        /// simultaneously (e.g., ShowChecked v ShowUnchecked radio buttons).
-        /// </remarks>
-        IDisposable BeginPredicateAtom();
-
-        IReadOnlyDictionary<string, Enum> ActiveFilters { get; }
-
-        void ActivatePredicates(Enum stdPredicate, params Enum[] more);
-
-        void DeactivatePredicates(Enum stdPredicate, params Enum[] more);
-
-        void ClearPredicates(bool clearInputText = true);
-    }
+#endif
 }
