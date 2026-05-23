4e2c58963d019403613a03a19e1ed777242ad8c2
Tom Gregor <tgregor@ivsoftware.com>
2026-05-23 12:29:09 -0600

    aspirational limits.

--- IVSoftware.Portable.SQLiteMarkdown.MSTest/TestClass_060523_OQFC.cs
+++ IVSoftware.Portable.SQLiteMarkdown.MSTest/TestClass_060523_OQFC.cs
@@ -25,6 +25,10 @@ public class TestClass_060523_OQFC
         {
             subtest_FilterBeforePopulate(omc);
         }
+        foreach (var omc in MakeOMCs(QueryFilterConfig.Filter))
+        {
+            subtest_FilterBeforeReplace(omc);
+        }
         #region S U B T E S T S
         void subtest_PopulateBeforeFilter(IModeledCollection<SelectableQFModel> omc)
         {
@@ -40,27 +44,28 @@ public class TestClass_060523_OQFC
             actual.ToClipboardExpected();
             { }
             expected = @" 
-[IME Len: 0, IsFiltering: False], [Net: 10, CC: 10, PMC: 0], [QueryAndFilter: SearchEntryState.Cleared, FilteringState.Ineligible]";
+[IME Len: 0, IsFiltering: True], [Net: 10, CC: 10, PMC: 0], [QueryAndFilter: SearchEntryState.QueryCompleteWithResults, FilteringState.Armed]";
 
             Assert.AreEqual(
                 expected.NormalizeResult(),
                 actual.NormalizeResult(),
-                "Expecting 10 items presented as query result."
+                "Expecting 10 items presented as query result." +
+                "MentalModel: 'New keystrokes will filter the visible collection'."
             );
             oqfs.QueryFilterConfig = QueryFilterConfig.Filter;
             actual = omc.StateReport();
             actual.ToClipboardExpected();
             { }
             expected = @" 
-[IME Len: 0, IsFiltering: True], [Net: 10, CC: 10, PMC: 0], [Filter: SearchEntryState.QueryCompleteNoResults, FilteringState.Armed]"
+[IME Len: 0, IsFiltering: True], [Net: 10, CC: 10, PMC: 0], [Filter: SearchEntryState.QueryCompleteWithResults, FilteringState.Armed]"
             ;
             Assert.AreEqual(
                 expected.NormalizeResult(),
                 actual.NormalizeResult(),
-                "Expecting 10 items presented as canonical filter source."
+                "Expecting 10 items presented as canonical filter source." +
+                "MentalModel: 'New keystrokes will filter the visible collection'."
             );
         }
-
         void subtest_FilterBeforePopulate(IModeledCollection<SelectableQFModel> omc)
         {
             IObservableQueryFilterSource<SelectableQFModel>
@@ -80,7 +85,45 @@ public class TestClass_060523_OQFC
             Assert.AreEqual(
                 expected.NormalizeResult(),
                 actual.NormalizeResult(),
+                "Expecting state reflects empty canon." +
+                "MentalModel: 'A filter-only collection is always either armed or active (even with 0 items).'"
+            );
+
+            omc.PopulateForDemo(10);
+            actual = omc.StateReport();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+[IME Len: 0, IsFiltering: True], [Net: 10, CC: 10, PMC: 0], [Filter: SearchEntryState.QueryCompleteWithResults, FilteringState.Armed]"
+            ;
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
+                "Expecting 10 items presented as canonical filter source."
+            );
+        }
+
+        void subtest_FilterBeforeReplace(IModeledCollection<SelectableQFModel> omc)
+        {
+            IObservableQueryFilterSource<SelectableQFModel>
+                oqfs = (IObservableQueryFilterSource<SelectableQFModel>)omc;
+            Assert.AreEqual(
+                QueryFilterConfig.Filter,
+                oqfs.QueryFilterConfig,
+                "Expecting valid cast to IOQFS and default config.");
+
+            actual = omc.StateReport();
+            actual.ToClipboardExpected();
+            { }
+            expected = @" 
+[IME Len: 0, IsFiltering: True], [Net: 0, CC: 0, PMC: 0], [Filter: SearchEntryState.QueryCompleteNoResults, FilteringState.Armed]"
+            ;
+
+            Assert.AreEqual(
+                expected.NormalizeResult(),
+                actual.NormalizeResult(),
                 "Expecting state reflects empty canon."
+                "MentalModel: 'A filter-only collection is always either armed or active (even with 0 items).'"
             );
 
             oqfs.ReplaceItems(default (IList<SelectableQFModel>).PopulateForDemo(10));
@@ -93,7 +136,8 @@ public class TestClass_060523_OQFC
             Assert.AreEqual(
                 expected.NormalizeResult(),
                 actual.NormalizeResult(),
-                "Expecting 10 items presented as canonical filter source."
+                "Expecting 10 items presented as canonical filter source." +
+                "MentalModel: 'New keystrokes will filter the visible collection'."
             );
         }
         #endregion S U B T E S T S
